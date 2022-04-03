060d568a84495c377b908f228e23e6e1edb89e0d
Dommiiii11 <domilegotovtaite@gmail.com>
2022-04-03 22:10:59 +0300

    addeed additional model

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,12 @@ class Post(models.Model):
     def was_published_recently(self):
         return self.release_date >= timezone.now() - datetime.timedelta(days=1)
 
+class Image(models.Model): 
+    title = models.CharField(max_length=200)
+    img = models.ImageField(upload_to="media")
+    country_name = models.ForeignKey(Country, on_delete=models.CASCADE)
+    class Meta:
+        db_table = "gallery"
 
 # country1 = Country(country_name="Greece", capital="Athens", places_to_visit="Cafes and restarants")
 # country1.save()
